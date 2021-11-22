770665d2b168881d726e0047b8a6474a373b7e0f
Jordan O'Hara (student) <jo372@uni.brighton.ac.uk>
2021-11-22 09:50:04 +0000

    creating a pin button that will be used within the pin input screen.

---
+++ src/components/pin/PinButton.tsx
@@ -0,0 +1,10 @@
+import { DetailsHTMLAttributes } from "react"
+
+interface PinButtonProps extends Omit<DetailsHTMLAttributes<HTMLButtonElement>, 'className'> {
+    text?: string | number
+}
+const PinButton : React.FC<PinButtonProps> = (props : PinButtonProps) => {
+    return <button className="pin-btn" id={props.id} onClick={props.onClick}>{props.text}</button>
+}
+
+export default PinButton;
