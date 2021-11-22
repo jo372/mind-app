6d232672baa8d852d1b52f4bc35d6969ff9436a8
Jordan O'Hara <J.O'Hara2@uni.brighton.ac.uk>
2021-11-22 15:08:36 +0000

    initial commit of the signup screen which is a 2 screen pin entry confirmation, which will later be used to store the users hashed pin code.

---
+++ src/screens/SignUp.tsx
@@ -0,0 +1,39 @@
+import { useState } from "react";
+import PinInput from "../components/pin/PinInput";
+
+interface SignUpProps {
+    onComplete ?: (hashed_pin: string) => void
+}
+
+const SignUp : React.FC<SignUpProps> = (props) => {
+    const [firstPinEntry, setFirstPinEntry] = useState<string>("");
+    const [error, setError] = useState<string | null>(null);
+
+    const checkPinEntriesMatch = (secondPinEntry: string) => {
+        const match = firstPinEntry === secondPinEntry;
+        if(!match) {
+            setError("Pin's do not match, please try again.");
+            setFirstPinEntry("");
+        } else {
+            setError("");
+        }
+
+    }
+
+    const displayFirstScreen = () => {
+        return <PinInput onSubmit={(hashed_pin) => setFirstPinEntry(hashed_pin)}></PinInput>
+    }
+
+    const displaySecondScreen = () => {
+        return <PinInput onSubmit={(hashed_pin) => checkPinEntriesMatch(hashed_pin)}></PinInput>;
+    }
+
+    return <div className="pin-entry-scrreen">
+        <p className="text-center error">{error}</p>
+        {
+            firstPinEntry === "" ?  displayFirstScreen(): displaySecondScreen()
+        }
+    </div>;
+}
+
+export default SignUp;
