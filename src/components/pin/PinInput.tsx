e62b0c72e60087f3edd20a52f88e7eace0d9dcec
Jordan O'Hara (student) <jo372@uni.brighton.ac.uk>
2021-11-22 11:17:16 +0000

    updated to include pin masking when the user is typing their pin.

--- src/components/pin/PinInput.tsx
+++ src/components/pin/PinInput.tsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from "react";
 import PinButton from "./PinButton";
 
 interface PinInputProps {
@@ -5,15 +6,27 @@ interface PinInputProps {
 }
 
 const PinInput : React.FC<PinInputProps> = (props) => {
+    const [input, setInput] = useState<string>("");
+
+    useEffect(() => {
+        const node : HTMLInputElement | null = document.querySelector('.pin-input-display');
+        if(node) {
+            node.value = Array.from(input).map(() => "●").join('');
+        }
+    }, [input]);
+
     const createPinButtons = () : JSX.Element[] => {
         const buttons : JSX.Element[] = new Array();
         for(let i=0; i < 10; i++) {
-            buttons.push(<PinButton key={`pin_button_${i}`} text={i}/>)
+            buttons.push(<PinButton key={`pin_button_${i}`} text={i} onClick={(e) => {
+                const target = e.target as HTMLButtonElement;
+                setInput(input + target.innerText)
+            }}/>);
         }
         return buttons.reverse();
     }
     return <div className="pin-input">
-        <input className="pin-input-display" type="text"/>
+        <input className="pin-input-display" type="text" disabled/>
         { createPinButtons() }
     </div>
 }
