51dd9eef5750f2ba492aae6bfe4a4eecf0177efc
Jordan O'Hara <J.O'Hara2@uni.brighton.ac.uk>
2021-11-23 22:21:50 +0000

    initial commit of the UserHomeScreenProps as well as adding functionality where the user can now use the arrow keys to select the right or left column.

--- src/screens/UserHome.tsx
+++ src/screens/UserHome.tsx
@@ -1,7 +1,31 @@
+import { useEffect } from "react";
 import { BiMessageAdd } from "react-icons/bi";
 import { CgTranscript } from "react-icons/cg";
 
+interface UserHomeScreenProps {
+    
+}
+
 const UserHomeScreen = () => {
+
+    useEffect(() => {
+        const onKeyDown = ({key}: KeyboardEvent) => {
+            key = key.toLowerCase();
+            let el : HTMLDivElement | null = document.querySelector(key === "arrowleft" ? '.left-column' : '.right-column');
+            if(el) { 
+                el.click(); 
+                el.classList.toggle('active');
+                setTimeout(() => { 
+                    el?.classList.toggle('active');
+                }, 100);
+            }
+        }
+        const body = document.body;
+
+        body.addEventListener('keydown', onKeyDown);
+        return () => body.removeEventListener('keydown', onKeyDown);
+    });
+
     return <div className="user-home">
         <div className="left-column">
             <div className="content">
