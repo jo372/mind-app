0b7da9131bb1b84b56b9de212183f53557106db5
Jordan O'Hara <J.O'Hara2@uni.brighton.ac.uk>
2021-11-23 22:45:30 +0000

    updated to to include the new add log entry and show log entry screen when the buttons are pressed.

--- src/screens/UserHome.tsx
+++ src/screens/UserHome.tsx
@@ -1,14 +1,19 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { BiMessageAdd } from "react-icons/bi";
 import { CgTranscript } from "react-icons/cg";
+import AddLogEntryScreen from "./AddLogEntry";
+import ShowLogEntriesScreen from "./ShowLogEntries";
 
-interface UserHomeScreenProps {
-    
+enum Section {
+    VIEW_ENTRIES,
+    WRITE_ENTRY 
 }
 
 const UserHomeScreen = () => {
-
-    useEffect(() => {
+    const [section, setSection] = useState<Section | null>(null);
+    const { VIEW_ENTRIES, WRITE_ENTRY } = Section;
+    
+    useEffect(() => { 
         const onKeyDown = ({key}: KeyboardEvent) => {
             key = key.toLowerCase();
             let el : HTMLDivElement | null = document.querySelector(key === "arrowleft" ? '.left-column' : '.right-column');
@@ -26,20 +31,31 @@ const UserHomeScreen = () => {
         return () => body.removeEventListener('keydown', onKeyDown);
     });
 
+    const showViewEntriesScreen = () => {
+        return <ShowLogEntriesScreen/>
+    }
+
+    const showAddLogEntryScreen = () => {
+        return <AddLogEntryScreen/>
+    }
+
+    if(section === VIEW_ENTRIES) return showViewEntriesScreen();
+    if(section === WRITE_ENTRY) return showAddLogEntryScreen();
+
     return <div className="user-home">
-        <div className="left-column">
-            <div className="content">
-                <CgTranscript />
-                <p>View entries</p>
+            <div className="left-column" onClick={ () => setSection(Section.VIEW_ENTRIES) }>
+                <div className="content">
+                    <CgTranscript />
+                    <p>View entries</p>
+                </div>
             </div>
-        </div>
-        <div className="right-column">
-            <div className="content">
-                <BiMessageAdd/>
-                <p>Add a new entry</p>
+            <div className="right-column" onClick={ () => setSection(Section.WRITE_ENTRY) }>
+                <div className="content">
+                    <BiMessageAdd/>
+                    <p>Add a new entry</p>
+                </div>
             </div>
-        </div>
-    </div>;
+        </div>;
 }
 
 export default UserHomeScreen;
