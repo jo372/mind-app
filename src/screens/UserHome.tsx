d2374638f968989e862d4de186dcdfc5344b3b12
Jordan O'Hara <J.O'Hara2@uni.brighton.ac.uk>
2021-11-23 16:10:04 +0000

    updated to include content within the columns, now the user is presented with a signpost where they can choose where to go within the application.

--- src/screens/UserHome.tsx
+++ src/screens/UserHome.tsx
@@ -1,9 +1,21 @@
+import { BiMessageAdd } from "react-icons/bi";
+import { CgTranscript } from "react-icons/cg";
 
 const UserHome = () => {
-    return <>
-        <div className="left-column"></div>
-        <div className="right-column"></div>
-    </>;
+    return <div className="user-home">
+        <div className="left-column">
+            <div className="content">
+                <CgTranscript />
+                <p>View entries</p>
+            </div>
+        </div>
+        <div className="right-column">
+            <div className="content">
+                <BiMessageAdd/>
+                <p>Add a new entry</p>
+            </div>
+        </div>
+    </div>;
 }
 
 export default UserHome;
