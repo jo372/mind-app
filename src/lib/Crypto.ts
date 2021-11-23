dc5626b836d8b1eb9a090553b6a08d513e1fdff1
Jordan O'Hara <J.O'Hara2@uni.brighton.ac.uk>
2021-11-23 11:19:35 +0000

    moved from Crypto to CryptoHelper to avoid confusion.

--- src/lib/Crypto.ts
+++
@@ -1,8 +0,0 @@
-import CryptoJS from "crypto-js";
-
-export default class Crypto {
-    public static createSha512Hash(val: string) : string {
-        const hash = CryptoJS.SHA512(val);
-        return hash.toString(CryptoJS.enc.Hex);
-    }
-}
