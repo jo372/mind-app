8b09f28f15d5e50f02e7a8f33addc78c8d91268a
Jordan O'Hara <J.O'Hara2@uni.brighton.ac.uk>
2021-11-23 18:57:54 +0000

    create a simple screen which is displayed to the user when unauthorized access happens. It tells them to go back, if we provide an onClick event handler, this will be called when the clicks the button.

---
+++ src/screens/UnauthorizedAccess.tsx
@@ -0,0 +1,14 @@
+interface UnAuthorizedAccessScreenProps {
+    onClick ?: () => void
+}
+
+const UnAuthorizedAccessScreen : React.FC<UnAuthorizedAccessScreenProps> = ({onClick}) => {
+    return <div className="container text-center">
+        <p className="error">Unauthorized Access, please try again.</p>
+        <button onClick={() => {
+            if(onClick) onClick();
+        }}>Go Back</button>
+    </div>
+}
+
+export default UnAuthorizedAccessScreen;
