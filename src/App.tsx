19025844336fa0aac1d688c5dc184f8befad2bea
Jordan O'Hara <J.O'Hara2@uni.brighton.ac.uk>
2021-11-23 11:18:50 +0000

    implemented the ability to register to the application through a pin, the next stage is to ask the user once again for the pin to confirm they are themselves. After registeration we will go straight to the user home screen.

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,28 @@
+import { useEffect, useState } from 'react';
 import './App.css';
+import CustomStorage from './lib/CustomStorage';
+import Key from './lib/Key';
 import SignUp from './screens/SignUp';
 
 function App() {
+  const [hasPin, setHasPin] = useState<boolean>(false);
+
+  useEffect(() => {
+      setHasPin(CustomStorage.hasKey(Key.PIN));
+  });
+
+  const showSetupScreen = () => {
+    return <SignUp onComplete={(hashed_pin) => {
+      CustomStorage.setKeyValue(Key.PIN, hashed_pin);
+      setHasPin(true);
+    }}/>;
+  }
+
+  const showUserHomeScreen = () => {
+    return <p>Successfully registered :)</p>;
+  }
   return (
-    <SignUp onComplete={(hashed_pin) => console.log(hashed_pin)}/>
+      !hasPin ? showSetupScreen() : showUserHomeScreen()
   );
 }
 
