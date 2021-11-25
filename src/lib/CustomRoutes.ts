186895f0e69ca31a2af68fc82de40583efe6e691
Jordan O'Hara <J.O'Hara2@uni.brighton.ac.uk>
2021-11-25 23:59:52 +0000

    a class which provides the string values of the custom routes, this is used so I can just change them in one place instead of multiple.

---
+++ src/lib/CustomRoutes.ts
@@ -0,0 +1,8 @@
+export default class CustomRoutes {
+    public static readonly REGISTER = "/register";
+    public static readonly HOME = "/home";
+    public static readonly UNAUTHORIZED = "/unauthorized";
+    public static readonly SHOW_LOGS = "/logs";
+    public static readonly ADD_LOG_ENTRY = "/add-log-entry";
+    public static readonly LOGIN = "/login";
+}
