23e753601081b9c41d6527f52bf764683b8562df
Jordan O'Hara <J.O'Hara2@uni.brighton.ac.uk>
2021-11-25 23:55:31 +0000

    updated to rerouting when the user isn't logged in as they should not be able to access these pages.

--- src/screens/AddLogEntry.tsx
+++ src/screens/AddLogEntry.tsx
@@ -1,5 +1,28 @@
+import { useEffect } from "react";
+import { BiArrowBack } from "react-icons/bi";
+import { useNavigate } from "react-router";
+import CustomRoutes from "../lib/CustomRoutes";
+import CustomStorage from "../lib/CustomStorage";
+import Key from "../lib/Key";
+
 const AddLogEntryScreen = () => {
-    return <><p>Add Log Entry Screen</p></>
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        if(CustomStorage.getValueByKey(Key.AUTHENTICATED) === "false") navigate(CustomRoutes.LOGIN);
+    });
+    
+    return <div className="container text-center">
+        <div  className="back-button">
+            <BiArrowBack onClick={() => {
+                navigate(CustomRoutes.HOME)
+            }}/>
+        </div>
+        <h1>Add Log Entry</h1>
+        <div className="container">
+
+        </div>
+    </div>
 }
 
 export default AddLogEntryScreen;
