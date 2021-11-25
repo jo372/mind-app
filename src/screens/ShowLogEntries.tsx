6454787cf58121cfb86b781af8c124fed8aa1547
Jordan O'Hara <J.O'Hara2@uni.brighton.ac.uk>
2021-11-25 23:55:58 +0000

    updated to include reroute when the user has not logged in.

--- src/screens/ShowLogEntries.tsx
+++ src/screens/ShowLogEntries.tsx
@@ -1,4 +1,14 @@
+import { useEffect } from "react";
+import { useNavigate } from "react-router";
+import CustomRoutes from "../lib/CustomRoutes";
+import CustomStorage from "../lib/CustomStorage";
+import Key from "../lib/Key";
+
 const ShowLogEntriesScreen = () => {
+    const navigate = useNavigate();
+    useEffect(() => {
+        if(CustomStorage.getValueByKey(Key.AUTHENTICATED) === "false") navigate(CustomRoutes.LOGIN);
+    })
     return <><p>Show Log Entry Screen</p></>
 }
 export default ShowLogEntriesScreen;
