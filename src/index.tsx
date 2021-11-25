83219021d471d510361fed275f630ad608528fa7
Jordan O'Hara <J.O'Hara2@uni.brighton.ac.uk>
2021-11-25 23:53:16 +0000

    updated to include react dom routing and pointing towards their respective pages.

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,43 @@
-import React from 'react';
 import ReactDOM from 'react-dom';
+import { Route, BrowserRouter as Router, Routes } from 'react-router-dom'
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import UserHomeScreen from './screens/UserHome';
+import SignUpScreen from './screens/SignUp';
+import UnAuthorizedAccessScreen from './screens/UnauthorizedAccess';
+import ShowLogEntriesScreen from './screens/ShowLogEntries';
+import AddLogEntryScreen from './screens/AddLogEntry';
+import CustomRoutes from './lib/CustomRoutes';
+import LoginScreen from './screens/LoginScreen';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const Routing = () => {
+  const { 
+    REGISTER, 
+    UNAUTHORIZED, 
+    LOGIN,
+    HOME, 
+    SHOW_LOGS, 
+    ADD_LOG_ENTRY 
+  } = CustomRoutes;
+
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<App/>} />
+        <Route path={REGISTER} element={<SignUpScreen/>} />
+        <Route path={LOGIN} element={<LoginScreen/>} />
+        <Route path={UNAUTHORIZED} element={<UnAuthorizedAccessScreen/>} />
+        <Route path={HOME} element={<UserHomeScreen/>} />
+        <Route path={SHOW_LOGS} element={<ShowLogEntriesScreen/>} />
+        <Route path={ADD_LOG_ENTRY} element={<AddLogEntryScreen/>} />
+      </Routes>
+    </Router>
+  );
+}
+
+ReactDOM.render(<Routing/>, document.getElementById('root'));
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
