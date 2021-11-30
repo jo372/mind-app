c8e5f1f6b392952f7aa7f53e5cfc8f29be3d822c
Jordan O'Hara <J.O'Hara2@uni.brighton.ac.uk>
2021-11-30 10:52:11 +0000

    updated back-button to a button for accessibility reasons. I've already created a displayLatLong() method which displays the users lat lon, the error or loading when the user hasn't selected an option yet.

--- src/screens/AddLogEntry.tsx
+++ src/screens/AddLogEntry.tsx
@@ -1,26 +1,43 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { BiArrowBack } from "react-icons/bi";
 import { useNavigate } from "react-router";
 import CustomRoutes from "../lib/CustomRoutes";
 import CustomStorage from "../lib/CustomStorage";
 import Key from "../lib/Key";
+import Weather from "../lib/Weather";
+import User from "../lib/User";
 
 const AddLogEntryScreen = () => {
     const navigate = useNavigate();
+    const [userCoord, setUserCoord] = useState<GeolocationPosition | undefined >(undefined);
+    const [error, setError] = useState<string | undefined>(undefined);
 
     useEffect(() => {
         if(CustomStorage.getValueByKey(Key.AUTHENTICATED) === "false") navigate(CustomRoutes.LOGIN);
-    });
+        
+        User.askForUserLocation(
+            (position: GeolocationPosition) => setUserCoord(position), 
+            (positionError: GeolocationPositionError) => setError(positionError.message)
+        )
+    }, [navigate]);
     
+    const displayLatLong = () => {
+        return <p>
+            {   
+                !userCoord && error ? error : 
+                !userCoord ? "Loading..." : 
+                `${userCoord.coords.latitude} ${userCoord.coords.longitude}`
+            }
+        </p>
+    }
+
     return <div className="container text-center">
-        <div  className="back-button">
-            <BiArrowBack onClick={() => {
-                navigate(CustomRoutes.HOME)
-            }}/>
-        </div>
+        <button className="back-button" onClick={() => navigate(CustomRoutes.HOME)}>
+            <BiArrowBack/>
+        </button>
         <h1>Add Log Entry</h1>
         <div className="container">
-
+           { displayLatLong() }
         </div>
     </div>
 }
