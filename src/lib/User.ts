b56e344f4b219f419f7ce0ea0c66307a0f8ddb9a
Jordan O'Hara <J.O'Hara2@uni.brighton.ac.uk>
2021-12-06 15:50:03 +0000

    updated from askForUserLocation to askForLocation as it made more sense to call User.askForLocation

--- src/lib/User.ts
+++ src/lib/User.ts
@@ -1,5 +1,5 @@
 export default class User {
-    public static askForUserLocation(onSuccess: PositionCallback, onError?: PositionErrorCallback | null | undefined) {
+    public static askForLocation(onSuccess: PositionCallback, onError?: PositionErrorCallback | null | undefined) {
         if(navigator.geolocation) {
             navigator.geolocation.getCurrentPosition(onSuccess, onError);
         }
