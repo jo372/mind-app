42aa40fefb09b0e2ebd09e448f709fde19b25fcf
Jordan O'Hara <J.O'Hara2@uni.brighton.ac.uk>
2021-12-06 12:52:08 +0000

    initial commit of a basic class that adds a loading icon from react-icons and includes the loading styling (which is the animation for spinning)

---
+++ src/components/LoadingIcon/LoadingIcon.tsx
@@ -0,0 +1,7 @@
+import { AiOutlineLoading3Quarters } from 'react-icons/ai';
+
+const LoadingIcon = () => {
+    return <AiOutlineLoading3Quarters className="loading"/>;
+}
+
+export default LoadingIcon;
