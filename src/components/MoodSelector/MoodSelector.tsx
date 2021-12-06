da4cdd0dea247410ecb84a6035496ca032cb8271
Jordan O'Hara <J.O'Hara2@uni.brighton.ac.uk>
2021-12-06 09:32:13 +0000

    updated the MoodSelectorProps onClick method to allow the return of undefined when the user unclicks a mood and no other mood has been selected.

--- src/components/MoodSelector/MoodSelector.tsx
+++ src/components/MoodSelector/MoodSelector.tsx
@@ -1,21 +1,40 @@
-enum Mood {
+import React from 'react';
+import { AiOutlineMeh } from 'react-icons/ai';
+import { RiEmotionHappyLine, RiEmotionUnhappyLine } from 'react-icons/ri';
+
+export enum Mood {
     Happy = "happy",
     Sad = "sad",
     Meh = "meh",  
 }
 
 interface MoodSelectorProps {
-    onClick: (mood: Mood) => void;
+    onClick: (mood: Mood | undefined) => void;
 }
 
 const MoodSelector = (props: MoodSelectorProps) => {
-    return (
-        <div className="mood-selector">
-            <button onClick={() => props.onClick(Mood.Happy)}>Happy</button>
-            <button onClick={() => props.onClick(Mood.Sad)}>Sad</button>
-            <button onClick={() => props.onClick(Mood.Meh)}>Meh</button>
-        </div>
-    );
+    const [activeElement, setActiveElement] = React.useState<HTMLButtonElement | undefined>();
+    const handleClick = (event: React.MouseEvent<HTMLButtonElement>, mood: Mood) => {
+        const prevActiveElement = document.querySelector(".btn.mood.active");
+        
+        const _activeElement = event.currentTarget as HTMLButtonElement;
+        setActiveElement(_activeElement);
+        
+        if(prevActiveElement && prevActiveElement === activeElement) {
+            prevActiveElement.classList.toggle('active');
+        }
+
+        if(_activeElement !== prevActiveElement) _activeElement.classList.toggle('active');
+
+        const anyMoodSelected = document.querySelector(".btn.mood.active");
+        
+        props.onClick(anyMoodSelected !== null ? mood : undefined);
+    }
+    return <div className="mood-selector">
+        <button className="btn mood" onClick={(e) => handleClick(e, Mood.Happy)}><RiEmotionHappyLine/></button>
+        <button className="btn mood" onClick={(e) => handleClick(e, Mood.Meh)}><AiOutlineMeh/></button>
+        <button className="btn mood" onClick={(e) => handleClick(e, Mood.Sad)}><RiEmotionUnhappyLine/></button>
+    </div>
 };
 
 export default MoodSelector;
