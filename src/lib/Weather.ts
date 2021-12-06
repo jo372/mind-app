6fcbb824b782ea91e2c2c3e2898aeebdf42a598f
Jordan O'Hara <J.O'Hara2@uni.brighton.ac.uk>
2021-12-06 12:49:55 +0000

    updated to include two new data types WeatherData and WeatherError which can be used as a return type in the App.tsx. Created a method which take the lat and long and checks if the user has provided a weather API key. If we successfully fetch the data parse it into the WeatherData which will transform it into a format I can use else return an error.

--- src/lib/Weather.ts
+++ src/lib/Weather.ts
@@ -1,3 +1,72 @@
+interface WeatherDataParams {
+    name: string,
+    main: {
+        feels_like: number;
+        humidity: number;
+        pressure: number;
+        temp: number;
+        temp_max: number;
+        temp_min: number;
+    },
+    weather : {
+        description: string;
+        icon: string;
+        main: string;
+    }
+}
+
+export class WeatherData {
+    public name: string;
+    public main: {
+        feels_like: number;
+        humidity: number;
+        pressure: number;
+        temp: number;
+        temp_max: number;
+        temp_min: number;
+    };
+    public weather : {
+        description: string;
+        icon: string;
+        main: string;
+    };
+    constructor(config: WeatherDataParams) {
+        this.name = config.name;
+        this.main = config.main;
+        this.weather = config.weather;
+    }
+}
+
+export class WeatherError extends Error {
+    constructor(message: string) {
+        super(message);
+        this.name = 'WeatherError';
+    }
+}
+
 export default class Weather {
-    
+    public static fromLatLon(lat: number, lon: number) : Promise<WeatherError | WeatherData> {
+        return new Promise((resolve, reject) => {
+            const WEATHER_API_KEY = process.env.REACT_APP_WEATHER_API_KEY;
+
+            if(!WEATHER_API_KEY) {
+                reject(new WeatherError("No weather API key provided"));
+            }
+            
+            const WEATHER_API_URL = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${WEATHER_API_KEY}&units=metric`;
+            
+            fetch(WEATHER_API_URL)
+            .then(res => res.json())
+            .then(data => {
+                const parsedData = {
+                    name: data.name,
+                    main: data.main,
+                    weather: data.weather[0]
+                } as WeatherDataParams;
+
+                resolve(new WeatherData(parsedData));
+            })
+            .catch(err => reject(new WeatherError(err)));
+        })
+    }
 }
