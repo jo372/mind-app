c7d5706c36f51c3e06f0116caa7e1beb68c7d8e4
Jordan O'Hara <J.O'Hara2@uni.brighton.ac.uk>
2021-12-06 12:51:27 +0000

    removed displayLatLong for now, possibly removed in the future. updated to use the new Weather lib to get the weather information for the user and display it to the screen. This also include a spinning loading icon for dynamically loading content.

--- src/screens/AddLogEntry.tsx
+++ src/screens/AddLogEntry.tsx
@@ -4,9 +4,10 @@ import { useNavigate } from "react-router";
 import CustomRoutes from "../lib/CustomRoutes";
 import CustomStorage from "../lib/CustomStorage";
 import Key from "../lib/Key";
-import Weather from "../lib/Weather";
+import Weather, { WeatherData, WeatherError } from "../lib/Weather";
 import User from "../lib/User";
 import MoodSelector, { Mood } from "../components/MoodSelector/MoodSelector";
+import LoadingIcon from "../components/LoadingIcon/LoadingIcon";
 
 const messages = [
     "How are you feeling?",
@@ -22,6 +23,7 @@ const AddLogEntryScreen = () => {
     const navigate = useNavigate();
     const [userCoord, setUserCoord] = useState<GeolocationPosition | undefined >(undefined);
     const [error, setError] = useState<string | undefined>(undefined);
+    const [weatherData, setWeatherData] = useState<WeatherData | undefined>(undefined);
 
     useEffect(() => {
         if(!CustomStorage.getValueByKey(Key.PIN)) navigate(CustomRoutes.REGISTER)
@@ -32,21 +34,52 @@ const AddLogEntryScreen = () => {
             (positionError: GeolocationPositionError) => setError(positionError.message)
         )
     }, [navigate]);
+
+    useEffect(() => {
+        if(userCoord && !error) {
+            const { latitude: lat, longitude: long } = userCoord.coords;
+            
+            Weather.fromLatLon(lat, long).then((res : WeatherData | WeatherError) => {
+                if(res instanceof WeatherData) setWeatherData(res);
+                else setError(res.message);
+            });
+        }
+    }, [userCoord]);
     
     const generateRandomMessage = () => {
         const randomIndex = Math.floor(Math.random() * messages.length);
         return messages[randomIndex];
     }
 
-    const displayLatLong = () => {
-        return <p>
-            {   
-                !userCoord && error ? error : 
-                !userCoord ? "Loading..." : 
-                `${userCoord.coords.latitude} ${userCoord.coords.longitude}`
-            }
-        </p>
-    }
+    // const displayLatLong = () => {
+    //     return <p>
+    //         {   
+    //             !userCoord && error ? error : 
+    //             !userCoord ? "Loading..." : 
+    //             `${userCoord.coords.latitude} ${userCoord.coords.longitude}`
+    //         }
+    //     </p>
+    // }
+
+    const displayWeatherData = () => { 
+
+        // {"name":"Brighton and Hove","main":{"temp":279.98,"feels_like":276.62,"temp_min":276.85,"temp_max":281.73,"pressure":1009,"humidity":86},"weather":{"id":802,"main":"Clouds","description":"scattered clouds","icon":"03d"}}
+        // const locationName = weatherData?.name;
+        // console.log(weatherData);
+        if(weatherData) {
+            const locationName = weatherData.name;
+            const iconId = weatherData.weather.icon;
+            const weatherDescription = weatherData.weather.description;
+            const tempature = weatherData.main.temp;
+            return <span>
+                <img src={`https://openweathermap.org/img/wn/${iconId}@2x.png`} alt={weatherDescription} />
+                <p>{locationName}</p>
+                <p>{tempature}&deg;C</p>
+            </span>
+        } else {
+            return <LoadingIcon/>
+        }
+    };
 
     return <div className="container text-center">
         <button className="back-button" onClick={() => navigate(CustomRoutes.HOME)}>
@@ -54,7 +87,7 @@ const AddLogEntryScreen = () => {
         </button>
         <h1>Add Log Entry</h1>
         <div className="container">
-           { displayLatLong() }
+            { displayWeatherData() }
            <MoodSelector onClick={(mood: Mood | undefined) => console.log(mood)}/>
            <textarea className="mood-entry-details" placeholder={
                React.useMemo(() => generateRandomMessage(), [])
