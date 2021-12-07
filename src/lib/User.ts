7a078157bcfecb4b5add5116b754d32e617fc452
Jordan O'Hara <J.O'Hara2@uni.brighton.ac.uk>
2021-12-07 10:55:18 +0000

    removed redunant class as I will not be needing to add anything else to it.

--- src/lib/User.ts
+++
@@ -1,7 +0,0 @@
-export default class User {
-    public static askForLocation(onSuccess: PositionCallback, onError?: PositionErrorCallback | null | undefined) {
-        if(navigator.geolocation) {
-            navigator.geolocation.getCurrentPosition(onSuccess, onError);
-        }
-    }
-}
