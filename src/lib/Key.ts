31417302c73529b07e64c86b4d1f7fa49279abba
Jordan O'Hara <J.O'Hara2@uni.brighton.ac.uk>
2021-12-08 11:40:34 +0000

    updated to include a localStorage key for the log entries.

--- src/lib/Key.ts
+++ src/lib/Key.ts
@@ -1,4 +1,5 @@
 export default class Key {
     public static readonly PIN = "hashed_pin"; 
     public static readonly AUTHENTICATED = "authenticated";
+    public static readonly LOG_ENTRIES = "log_entries";
 }
